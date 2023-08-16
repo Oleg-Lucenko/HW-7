897f655a5b0989cc1005d7170ffc630c1c097869
Oleg-Lucenko <lucenko5577@gmail.com>
2023-08-17 01:42:44 +0300

    HW-7

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Counter from './components/Counter.jsx';
+import { createContext } from 'react';
+
+export const Context = createContext();
 
 function App() {
+  
+
+  const initialState= {counter : 0};
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Context.Provider value = {initialState}>
+      <div className="App">
+        <Counter />
+      </div>
+    </Context.Provider>
   );
 }
 
